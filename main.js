abd78ac53a8b6524071df46e8d8eb5615c332d4a
Unknown <cp4r3z@gmail.com>
2017-12-28 19:23:41 -0600

    clean console

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ module.exports.loop = function () {
     }
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
+    //console.log('Harvesters: ' + harvesters.length);
 
     if(harvesters.length < 2) {
         var newName = 'Harvester' + Game.time;
