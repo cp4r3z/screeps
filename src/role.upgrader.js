d4da41a09c1a33230f530e03262161cfe79ecc35
cp4r3z <cp4r3z@gmail.com>
2018-01-01 17:24:29 -0600

    send upgrader to nearest energy source

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -3,24 +3,31 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if(creep.memory.upgrading && creep.carry.energy === 0) {
+        if (creep.memory.upgrading && creep.carry.energy === 0) {
             creep.memory.upgrading = false;
             creep.say('harvesting');
         }
-        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
             creep.memory.upgrading = true;
             creep.say('upgrading');
         }
 
-        if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+        if (creep.memory.upgrading) {
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
             }
-        }
-        else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+        } else {
+            // find nearest source (with energy)
+            // use findPath to sort
+            var sources = creep.room.find(FIND_SOURCES, {
+                filter: (source) => {
+                    return source.energy > 0;
+                }
+            });            
+            var closestSource = sources.sort((sourceA, sourceB) => creep.room.findPath(creep.pos, sourceA.pos) - creep.room.findPath(creep.pos, sourceB.pos))[0];
+
+            if (creep.harvest(closestSource) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(closestSource, { visualizePathStyle: { stroke: '#ffaa00' } });
             }
         }
     }
