388dba5b6d15fffb2b662f2f123ddedc3763c52c
cp4r3z <cp4r3z@gmail.com>
2018-01-03 00:59:51 +0000

    Fly, my pretties!

---
+++ src/role.killer.js
@@ -0,0 +1,14 @@
+var roleKiler = {
+
+    /** @param {Creep} creep **/
+    run(creep) {
+        const target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (target) {
+            if (creep.attack(target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+        }
+    }
+};
+
+module.exports = roleKiller;
