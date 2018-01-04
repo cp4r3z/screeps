92d12cd106cb1b7c895f917660a66fe46134715f
cp4r3z <cp4r3z@gmail.com>
2018-01-04 01:13:05 +0000

    upgrade if done harvesting

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -1,3 +1,5 @@
+var roleUpgrader = require('role.upgrader');
+
 var roleHarvester = {
 
     /** @param {Creep} creep **/
@@ -19,6 +21,9 @@ var roleHarvester = {
                 if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
                 }
+            } else {
+                //upgrade
+                roleUpgrader.run(creep);
             }
         }
     }
