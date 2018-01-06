093b7184e6d8b0f34266f099b12b869f1040fcea
cp4r3z <cp4r3z@gmail.com>
2018-01-06 21:44:58 +0000

    Fill the container

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -15,6 +15,7 @@ var roleHarvester = {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION ||
                         structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_CONTAINER ||
                         structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
                 }
             });
