5cea3aedc8f4b0355ec4a758b5fc45f278fec298
cp4r3z <cp4r3z@gmail.com>
2018-01-07 04:07:16 +0000

    Giving a room a name

--- src/role.scout.js
+++ src/role.scout.js
@@ -3,7 +3,7 @@ const roleScout = {
         if (creep.room.controller.my) {
             // exit to the room specified in dest
             if (creep.memory.dest) {
-                const route = Game.map.findRoute(creep.room, anotherRoomName);
+                const route = Game.map.findRoute(creep.room, creep.memory.dest);
                 if (route.length > 0) {
                     //console.log('Now heading to room ' + route[0].room);
                     const exit = creep.pos.findClosestByRange(route[0].exit);
