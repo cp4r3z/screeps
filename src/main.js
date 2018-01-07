3128b267e5ba5ce69c4e47cdd5a16c01b68cba1c
cp4r3z <cp4r3z@gmail.com>
2018-01-07 14:57:11 -0600

    Log the total energy of the sytem. Oh! It'd be cool to have a "system info" debug message

--- src/main.js
+++ src/main.js
@@ -11,6 +11,12 @@ const CONFIG = require('config'),
 module.exports.loop = function() {
 
     const spawnName = 'Spawn1', // eventually this will have to be independent...
+
+        spawnEnergy = Game.spawns[spawnName].energy,
+        extensions = Game.spawns[spawnName].room.find(FIND_MY_STRUCTURES, {
+            filter: { structureType: STRUCTURE_EXTENSION }
+        }),
+        totalEnergy = spawnEnergy + extensions.reduce((total, structure) => total + structure.store[RESOURCE_ENERGY]),
         // all these need to get pushed into a config file. UGLY
         SPAWN_PROPS = {
             harvesters: {
@@ -30,22 +36,24 @@ module.exports.loop = function() {
             // odd that these things aren't arrays of strings... shrug
             parts: {
                 carry_fast: [WORK, CARRY, MOVE, MOVE], // = 100 + 50*3 = 250
-                carry_big: [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE], // = 100*4 + 50*9 = 850
+                carry_big: [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE], // = 100*3 + 50*9 = 750
                 kill: [TOUGH, ATTACK, MOVE, MOVE]
             }
 
         };
+
+    console.log(`Total Energy: ${totalEnergy}`);
     var tower = Game.getObjectById('5a51aac196005e55af510071');
-    if(tower) {
+    if (tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
         });
-        if(closestDamagedStructure) {
+        if (closestDamagedStructure) {
             tower.repair(closestDamagedStructure);
         }
 
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
+        if (closestHostile) {
             tower.attack(closestHostile);
         }
     }
@@ -63,8 +71,7 @@ module.exports.loop = function() {
             'Spawning a new ' + spawningCreep.memory.role,
             Game.spawns[spawnName].pos.x + 1,
             Game.spawns[spawnName].pos.y, { align: 'left', opacity: 0.8 });
-    }
-    else {
+    } else {
         // I feel like there's a way to use lodash to create a sorted "creeps" object
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
@@ -84,27 +91,23 @@ module.exports.loop = function() {
             // var towers = Game.rooms[roomName].find(
             //     FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
             // towers.forEach(tower => tower.attack(hostiles[0]));
-        }
-        else if (harvesters.length < SPAWN_PROPS.harvesters.min) {
+        } else if (harvesters.length < SPAWN_PROPS.harvesters.min) {
             //calculate spawn energy WITH the extensions
             if (Game.spawns[spawnName].energy >= 850) {
                 newName = 'HarvesterHeavy' + Game.time;
                 console.log('Attempting to spawn new big harvester: ' + newName);
                 Game.spawns[spawnName].spawnCreep(CREEP_PROPS.parts.carry_big, newName, { memory: { role: 'harvester' } });
-            }
-            else {
+            } else {
                 newName = 'Harvester' + Game.time;
                 console.log('Attempting to spawn new harvester: ' + newName);
                 Game.spawns[spawnName].spawnCreep(CREEP_PROPS.parts.carry_fast, newName, { memory: { role: 'harvester' } });
             }
 
-        }
-        else if (upgraders.length < SPAWN_PROPS.upgraders.min) {
+        } else if (upgraders.length < SPAWN_PROPS.upgraders.min) {
             newName = 'Upgrader' + Game.time;
             console.log('Attempting to spawn new upgrader: ' + newName);
             Game.spawns[spawnName].spawnCreep(CREEP_PROPS.parts.carry_fast, newName, { memory: { role: 'upgrader' } });
-        }
-        else if (builders.length < SPAWN_PROPS.builders.min) {
+        } else if (builders.length < SPAWN_PROPS.builders.min) {
             newName = 'Builder' + Game.time;
             console.log('Attempting to spawn new builder: ' + newName);
             Game.spawns[spawnName].spawnCreep(CREEP_PROPS.parts.carry_fast, newName, { memory: { role: 'builder' } });
