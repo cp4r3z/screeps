3b54d64e71e4f31fa03601046fc2412af49878d4
cp4r3z <cp4r3z@gmail.com>
2018-01-07 01:18:26 +0000

    Hold your position!

--- src/role.builder.js
+++ src/role.builder.js
@@ -53,7 +53,7 @@ var roleBuilder = {
                         }
             */
             //var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            const target = creep.room.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+            const target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
             if (target) {
                 if (creep.build(target) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
