e385deeb6166778da65722c778e8e83d72b807b0
cp4r3z <cp4r3z@gmail.com>
2018-01-07 14:12:05 -0600

    Ok, make the upgraders go to extensions too.

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -16,38 +16,45 @@ var roleUpgrader = {
             if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
             }
-        }
-        else {
+        } else {
             // find nearest source (with energy)
             // use findPath to sort
 
             // this shit needs to be a module or something.
             let sources =
                 Game.spawns['Spawn1'].room.find(FIND_SOURCES_ACTIVE)
-                
-                // Okay, I'm not sure you can harvest from extensions! Ha! Maybe we'll have to drop energy.
-                // Oh, SO, there's thie "withdraw" command that we should look into. Again, this needs to be a util.
-                /*
-                .concat(
-                    Game.spawns['Spawn1'].room.find(FIND_MY_STRUCTURES, {
-                        filter: (structure) => {
-                            return (structure.structureType == STRUCTURE_EXTENSION) && structure.energy > 0; //||
-                            //structure.structureType == STRUCTURE_CONTAINER);
-                        }
-                    })
-                );
-                */
+
+            // Okay, I'm not sure you can harvest from extensions! Ha! Maybe we'll have to drop energy.
+            // Oh, SO, there's thie "withdraw" command that we should look into. Again, this needs to be a util.
+
+            .concat(
+                Game.spawns['Spawn1'].room.find(FIND_MY_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION) && structure.energy > 0; //||
+                        //structure.structureType == STRUCTURE_CONTAINER);
+                    }
+                })
+            );
+
             //console.log(sources);
-            
+
             // ok, this is not efficient anymore. use the built in method instead
             const closestSource = sources.sort((sourceA, sourceB) => {
                 return creep.room.findPath(creep.pos, sourceA.pos).length - creep.room.findPath(creep.pos, sourceB.pos).length;
             })[0];
             //console.log(closestSource);
-            if (creep.harvest(closestSource) == ERR_NOT_IN_RANGE) {
-                //console.log(`role.upgrader: ${creep.name} moving to ${closestSource}.`)
-                creep.moveTo(closestSource, { visualizePathStyle: { stroke: '#ffaa00' } });
+            if (closestSource.structureType == STRUCTURE_EXTENSION) {
+                if (creep.withdraw(closestSource, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(closestSource, { visualizePathStyle: { stroke: '#ffaa00' } });
+                }
+            } else {
+                if (creep.harvest(closestSource) == ERR_NOT_IN_RANGE) {
+                    //console.log(`role.upgrader: ${creep.name} moving to ${closestSource}.`)
+                    creep.moveTo(closestSource, { visualizePathStyle: { stroke: '#ffaa00' } });
+                }
             }
+
+
         }
     }
 };
