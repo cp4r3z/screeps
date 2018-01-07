81e808fe5586ac8eab1f399de02c54ad1e80895a
cp4r3z <cp4r3z@gmail.com>
2018-01-07 02:35:30 +0000

    upgrading upgrader again and again

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -22,20 +22,20 @@ var roleUpgrader = {
             // use findPath to sort
 
             // this shit needs to be a module or something.
-            const sources = [].concat(
-                Game.spawns['Spawn1'].room.find(FIND_SOURCES_ACTIVE),
-                Game.spawns['Spawn1'].room.find(FIND_MY_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION) && structure.energy > 0; //||
-                        //structure.structureType == STRUCTURE_CONTAINER);
-                    }
-                })
-            );
-
+            let sources =
+                Game.spawns['Spawn1'].room.find(FIND_SOURCES_ACTIVE).concat(
+                    Game.spawns['Spawn1'].room.find(FIND_MY_STRUCTURES, {
+                        filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_EXTENSION) && structure.energy > 0; //||
+                            //structure.structureType == STRUCTURE_CONTAINER);
+                        }
+                    })
+                );
+            //console.log(sources);
             const closestSource = sources.sort((sourceA, sourceB) => {
                 return creep.room.findPath(creep.pos, sourceA.pos) - creep.room.findPath(creep.pos, sourceB.pos);
             })[0];
-
+            //console.log(closestSource);
             if (creep.harvest(closestSource) == ERR_NOT_IN_RANGE) {
                 //console.log(`role.upgrader: ${creep.name} moving to ${closestSource}.`)
                 creep.moveTo(closestSource, { visualizePathStyle: { stroke: '#ffaa00' } });
