06cfa00cde682a447f89c7cf3372debce6b5dfe3
cp4r3z <cp4r3z@gmail.com>
2018-01-15 22:01:46 -0600

    Removing garbage code in creep logic. Replacing with new garbage code.

--- src/logic.creep.js
+++ src/logic.creep.js
@@ -7,15 +7,6 @@ const role = {
     killer: require('./role.killer')
 }
 
-/*
-const roleHarvester = require('./role.harvester'),
-    roleUpgrader = require('./role.upgrader'),
-    roleBuilder = require('./role.builder'),
-    roleBuilderSpawn = require('./role.builder.spawn'),
-    roleScout = require('./role.scout'),
-    roleKiller = require('./role.killer');
-*/
-
 module.exports = () => {
     const rolesByRank = [
         'killer',
@@ -26,39 +17,7 @@ module.exports = () => {
         'scout'
     ];
 
-    function sortByRank(a, b) {
-        const creepRoleA = Game.creeps[a].memory.role,
-            creepRoleB = Game.creeps[b].memory.role,
-            roleRankA = rolesByRank.indexOf(creepRoleA),
-            roleRankB = rolesByRank.indexOf(creepRoleB);
-        if (roleRankA > 0 || roleRankB > 0) {
-            return roleRankA - roleRankB;
-        } else {
-            console.log('creep logic: bad sort');
-            return 1;
-        }
-    }
+    const creepsRanked = _.sortBy(Game.creeps, [(creep) => rolesByRank.indexOf(Game.creeps[creep].memory.role)]);
 
-    const creepsRanked = Game.creeps.sort(sortByRank);
-
-    _foreach(creepsRanked, (creep) => role[creep.memory.role].run(creep));
-
-    /*
-    for (const name in creepsRanked) {
-        const creep = Game.creeps[name];
-        if (creep.memory.role == 'killer') {
-            roleKiller.run(creep);
-        } else if (creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        } else if (creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        } else if (creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        } else if (creep.memory.role == 'builderspawn') {
-            roleBuilderSpawn.run(creep);
-        } else if (creep.memory.role == 'scout') {
-            roleScout.run(creep);
-        }
-    }
-    */
+    _.forEach(creepsRanked, (creep) => role[creep.memory.role].run(creep));
 };
