378ed4738a2c01c8ccfe0174e88e8a7d4ddfd738
cp4r3z <cp4r3z@gmail.com>
2018-01-18 19:09:39 -0600

    containers store more than energy now

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -46,7 +46,7 @@ var roleHarvester = {
                 target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity) ||
-                            (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] < structure.storeCapacity);
+                            (structure.structureType == STRUCTURE_CONTAINER && _.sum(structure.store) < structure.storeCapacity);
                     }
                 });
             }
