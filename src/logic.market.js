63520d0a6710fdc63d1f5d5f07e569f36e938105
cp4r3z <cp4r3z@gmail.com>
2018-01-21 21:05:37 +0000

    Dont run the market unless theres a terminal. Also, dont run.

--- src/logic.market.js
+++ src/logic.market.js
@@ -1,6 +1,8 @@
 logicTerminal = require('./logic.terminal');
 module.exports = {
     sales(roomName) {
+        if (!Game.rooms[roomName].terminal) return false;
+        
         //eventually, we can dynamically create / modify orders... specifically extend existing
         // for now, create an order if there's no active order and the resources is > 100
         // we also need energy right?
@@ -9,10 +11,13 @@ module.exports = {
         //CPU - getAllOrders takes a lot, I guess.
         const myOrders = Game.market.getAllOrders(order => order.type == ORDER_SELL && order.roomName == roomName);
         if (myOrders.length > 1) {
-            console.log(`market: There are too many orders!`);
+            //console.log(`market: There are too many orders!`);
         } else if (myOrders.length === 0) {
             // Create a new order
-            const terminal = Game.rooms[roomName].find(FIND_STRUCTURES, { filter: structure => structure.structureType == STRUCTURE_TERMINAL });
+            //const terminal = Game.rooms[roomName].find(FIND_STRUCTURES, { filter: structure => structure.structureType == STRUCTURE_TERMINAL });
+            const terminal = Game.rooms[roomName].terminal
+            console.log(terminal);
+            console.log(terminal.storeCapacity)
             const hasEnergy = terminal.store[RESOURCE_ENERGY] > 3000;
             //TODO: Hardcoded resource!!!
             const hasResource = terminal.store[RESOURCE_ZYNTHIUM] > resourceThreshold;
@@ -23,7 +28,7 @@ module.exports = {
                 // Figure out the "going rate"
                 const price = 0.7;
                 const totalAmount = resourceThreshold;
-                Game.market.createOrder(ORDER_SELL, RESOURCE_ZYNTHIUM, price, totalAmount, roomName);
+                //Game.market.createOrder(ORDER_SELL, RESOURCE_ZYNTHIUM, price, totalAmount, roomName);
             }
 
         } else {
