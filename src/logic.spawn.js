59a55e2df8b69879c3ea6b122a3b41876ae21408
cp4r3z <cp4r3z@gmail.com>
2018-01-21 07:47:35 -0600

    run away tower

--- src/logic.spawn.js
+++ src/logic.spawn.js
@@ -44,7 +44,7 @@ module.exports = (spawnName) => {
     // Uncomment this to see the total energy available for spawning. I need a debug module.
     //console.log(`Spawn: ${spawnName}, Total Energy: ${totalEnergy}/${totalCapacity}`);
 
-    // TOWER LOGIC
+    // TOWER LOGIC -- this can be its own thing
 
     const hostiles = Game.spawns[spawnName].room.find(FIND_HOSTILE_CREEPS);
 
@@ -57,7 +57,7 @@ module.exports = (spawnName) => {
             tower.attack(closestHostile);
         } else {
             const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax
+                filter: (structure) => structure.hits < structure.hitsMax && structure.hits < 1e5 // Um, this should probably be thought about.
             });
             if (closestDamagedStructure) {
                 tower.repair(closestDamagedStructure);
