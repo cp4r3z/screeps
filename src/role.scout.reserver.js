318c7fae807403116b2ed59b8fdbcb76b22dfced
cp4r3z <cp4r3z@gmail.com>
2018-02-25 14:35:34 -0600

    Fixes to the scout in preparation for actual remote harvesting

--- src/role.scout.reserver.js
+++ src/role.scout.reserver.js
@@ -1,18 +1,22 @@
 const roleScout = {
     run(creep) {
+
+        // Get the room status from memory
+        const roomMemory = Memory.rooms[creep.room.name];
+
+        // Load the base creep module
+        const base = require('./role.base')(creep, roomMemory);
+
         if (creep.room.name == creep.memory.dest) {
-            if (creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
+            target = Game.rooms[creep.memory.dest].controller;
+            if (creep.reserveController(target) == ERR_NOT_IN_RANGE) {
+                base.utils.movement.toDest(creep, target, 50);
             }
         } else {
-            // exit to the room specified in dest
-            const route = Game.map.findRoute(creep.room, creep.memory.dest);
-            if (route.length > 0) {
-                //console.log('Now heading to room ' + route[0].room);
-                const exit = creep.pos.findClosestByRange(route[0].exit);
-                creep.moveTo(exit);
-            }
+            target = new RoomPosition(24, 24, creep.memory.dest);
+            base.utils.movement.toDest(creep, target, 50);
         }
+        
     }
 }
 
